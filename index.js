fcc78a9587d59beedf0b1c5fc7e17df05122794b
clindsay107 <clindsay107@gmail.com>
2016-08-01 15:22:55 -0700

    Add basic web interface setup and scan form

--- index.js
+++ index.js
@@ -5,8 +5,36 @@ const Scanner = require('./lib/scan');
 const Mapper  = require('./lib/maps');
 const Texter  = require('./lib/text');
 const config  = require('./config.json');
+const express = require('express');
 
-console.log(`Notifications set to: ${_.join(config.notifications, ', ')}`);
-Scanner(config.account, config.pokemon, config.notifications, (targetsFound) => {
-  console.log(targetsFound)
+const server = express();
+server.set('views', __dirname + '/views');
+server.set('view engine', 'ejs');
+server.use(require('body-parser').urlencoded({ extended: false }));
+
+server.use((req, res, next) => {
+  console.log('[' + res.statusCode + ' ' + req.method + '] -> ' + req.hostname + req.path);
+  next();
 });
+
+server.get('/scan', (req, res) => {
+  res.render('scan');
+});
+
+server.post('/scan', (req, res) => {
+  let pokemon = _.split(req.body.poke, ', ');
+  console.log(pokemon)
+  Scanner(config.account, pokemon, config.notifications, (targetsFound) => {
+    console.log(targetsFound);
+  });
+  res.send('Searching for ' + pokemon)
+});
+
+server.listen(3000, () => {
+  console.log('Server started on port: ' + 3000);
+});
+
+// console.log(`Notifications set to: ${_.join(config.notifications, ', ')}`);
+// Scanner(config.account, config.pokemon, config.notifications, (targetsFound) => {
+//   console.log(targetsFound)
+// });
